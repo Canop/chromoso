d2b8d40171d59432ae212a243dc20fc21f6f1d82
Canop <cano.petrole@gmail.com>
2015-05-29 08:49:13 +0200

    Better information for the user

--- src/chrome/cso-main.js
+++ src/chrome/cso-main.js
@@ -1,60 +1,102 @@
 
-// --------------------------------------------
-// removing the ad pushing away the questions
-// --------------------------------------------
-var ad = document.querySelector('.everyonelovesstackoverflow'); // yes, only the first one is bad
-if (ad) {
-	ad.style.display='none';
-	console.log("Chromoso: killed ad");
-}
-
-// --------------------------------------------
-// removing or flagging bad questions
-// --------------------------------------------
 var badQuestionPatterns = [
 	/document\.write/,
-	/beginner/, /newbie/, /plz/i, /please/i, /\bsir\b/i, /urgent/i
+	/beginner/, /newbie/, /plz/i, /please/i, /\bsir\b/i, /urgent/i, /bear with me/i,
+	/bootstrap/i, /carousel/i
 ];
 var probablyBadQuestionPatterns = [
-	/not working/i, 
-	/bootstrap/i
+	/not working/i,
+	/thanks/i, /help/i
 ];
-function checkSummaryNode(node){
-	var text = node.textContent;
-	console.log("checking question ", node.id.split('-').pop());
-	for (var k=0; k<badQuestionPatterns.length; k++) {
-		if (badQuestionPatterns[k].test(text)) {
-			setTimeout(function(){
-				console.log("killed question", text);
-				node.remove();
-			}, 0);
-			return;
-		}
-	}
-	for (var k=0; k<probablyBadQuestionPatterns.length; k++) {
-		if (probablyBadQuestionPatterns[k].test(text)) {
-			var warningDiv = document.createElement("div");
-			warningDiv.appendChild(document.createTextNode("warning: "+probablyBadQuestionPatterns[k]));
-			warningDiv.setAttribute('style', "position:absolute;left:20px;top:-2px;z-index:24;color:red;");
-			node.style.position = "relative";
-			node.insertBefore(warningDiv, node.childNodes[0]);
-			return;
-		}
-	}
+
+
+// --------------------------------------------
+// utils
+// --------------------------------------------
+HTMLElement.prototype.prepend = function(node){
+	this.insertBefore(node, this.childNodes[0]);
 }
 
-// removing the questions initially in the page
-[].forEach.call(document.querySelectorAll('#questions .question-summary'), checkSummaryNode);
+// --------------------------------------------
+// info display
+// --------------------------------------------
+var chromosoLog = document.createElement("div");
+chromosoLog.id = "chromoso-log";
+document.body.prepend(chromosoLog);
+function log(h, t){
+	if (!t) t = 13000;
+	var e = document.createElement("div");
+	e.innerHTML = h;
+	chromosoLog.prepend(e);
+	setTimeout(function(){
+		e.className = "chromoso-dying";
+	}, t);
+	setTimeout(function(){
+		e.remove();
+	}, t+3000);
+}
 
-// removing new questions in the flow
-(new MutationObserver(function(mutations){
-	for (var i=0; i<mutations.length; i++) {
-		var nodes = mutations[i].addedNodes;
-		if (!nodes || !nodes.length) continue;
-		for (var j=0; j<nodes.length; j++) {
-			var	node = nodes[j];
-			if (!/\bquestion-summary\b/.test(node.className)) continue;
-			checkSummaryNode(node);
+if (document.getElementById('questions')) { // we're in a questions page
+	
+	log("Chromoso questions filtering is ON", 4000);
+
+	// --------------------------------------------
+	// 
+	// --------------------------------------------
+
+	// --------------------------------------------
+	// removing the ad pushing away the questions
+	// --------------------------------------------
+	var ad = document.querySelector('.everyonelovesstackoverflow'); // yes, only the first one is bad
+	if (ad) {
+		ad.style.display='none';
+		log("killed painful ad");
+	}
+
+	// --------------------------------------------
+	// removing or flagging bad questions
+	// making question links open in _blank
+	// --------------------------------------------
+	function visitSummaryNode(node){
+		var text = node.textContent;
+		var link = node.querySelector('.question-hyperlink');
+		link.target = "_blank";
+		console.log("checking question ", node.id.split('-').pop());
+		for (var k=0; k<badQuestionPatterns.length; k++) {
+			if (badQuestionPatterns[k].test(text)) {
+				setTimeout(function(){
+					link.title = "Trigger: " + badQuestionPatterns[k];
+					log("killed question " + link.outerHTML);
+					node.remove();
+				}, 0);
+				return;
+			}
+		}
+		for (var k=0; k<probablyBadQuestionPatterns.length; k++) {
+			if (probablyBadQuestionPatterns[k].test(text)) {
+				var warningDiv = document.createElement("div");
+				warningDiv.className = "chromoso-warning";
+				warningDiv.appendChild(document.createTextNode("warning: "+probablyBadQuestionPatterns[k]));
+				node.prepend(warningDiv);
+				return;
+			}
 		}
 	}
-})).observe(document.getElementById('questions'), {childList:true});
+
+	// removing the questions initially in the page
+	[].forEach.call(document.querySelectorAll('#questions .question-summary'), visitSummaryNode);
+
+	// removing new questions in the flow
+	(new MutationObserver(function(mutations){
+		for (var i=0; i<mutations.length; i++) {
+			var nodes = mutations[i].addedNodes;
+			if (!nodes || !nodes.length) continue;
+			for (var j=0; j<nodes.length; j++) {
+				var	node = nodes[j];
+				if (!/\bquestion-summary\b/.test(node.className)) continue;
+				visitSummaryNode(node);
+			}
+		}
+	})).observe(document.getElementById('questions'), {childList:true});
+	
+}
